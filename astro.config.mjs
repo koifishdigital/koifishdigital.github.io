4208efe2b6a1f440cd193e0237d41d723e57fce9
Cody Nguyen <36778689+CodyKN@users.noreply.github.com>
2025-09-29 14:23:32 -0500

    Change build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
 import { DEFAULT_LOCALE, LOCALES } from "./src/i18n/utils";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321/",
-  // todo - add site for sitemap
+  site: "https://koifish.digital",
   build: {
     format: "directory",
   },
@@ -25,6 +23,4 @@ export default defineConfig({
     alpinejs({ entrypoint: "/src/entrypoint" }),
   ],
   prefetch: true,
-  output: "server",
-  adapter: vercel(),
 });
